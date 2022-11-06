a4aa758ed137bf2826c53bac68148c3f3f72e197
k3vinrich4rd <kevinrichardve@gmail.com>
2022-11-06 13:15:36 -0300

    Inserindo escopo do projeto

---
+++ src/main/java/com/example/GitGitHub/GitGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitGitHub/GitGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
